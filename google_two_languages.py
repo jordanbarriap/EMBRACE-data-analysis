78f12bf290df103cba697dff507ba504607f4d4a
Kangying <conniezi1008@gmail.com>
2022-07-04 15:40:30 -0400

    use a real audio to test google 2 languages, the result is mediocre

--- google_two_languages.py
+++ google_two_languages.py
@@ -1,10 +1,12 @@
 from google.cloud import speech_v1p1beta1 as speech
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/kangying/Desktop/KEY.json"
 
 client = speech.SpeechClient()
 
-speech_file = "two.wav"
-first_lang = "en-US"
-second_lang = "es"
+speech_file = "/home/kangying/Desktop/Code/AudioProcessing/audios/record-672279722.51811.wav"
+first_lang = "es"
+second_lang = "en-US"
 
 with open(speech_file, "rb") as audio_file:
     content = audio_file.read()
@@ -17,13 +19,20 @@ config = speech.RecognitionConfig(
     audio_channel_count=2,
     language_code=first_lang,
     alternative_language_codes=[second_lang],
+    #enable_speaker_diarization=True,
 )
 
 print("Waiting for operation to complete...")
 response = client.recognize(config=config, audio=audio)
 
+f = open("google_2lang_output.txt", "w")
+
+
 for i, result in enumerate(response.results):
     alternative = result.alternatives[0]
-    print("-" * 20)
-    print(u"First alternative of result {}: {}".format(i, alternative))
-    print(u"Transcript: {}".format(alternative.transcript))
+    f.write("-" * 20)
+    f.write("\n")
+    f.write(u"First alternative of result {}: {} \n".format(i, alternative))
+    f.write(u"Transcript: {} \n".format(alternative.transcript))
+
+f.close()
