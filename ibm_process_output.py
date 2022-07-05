7257804a8d419e4489d9bd3708a654db2e87d2a9
Kangying <conniezi1008@gmail.com>
2022-07-05 17:04:41 -0400

    successfully sorted out utterances based on timepoints

--- ibm_process_output.py
+++ ibm_process_output.py
@@ -9,16 +9,31 @@ with open('ibm_transcript.json') as json_file:
 a speaker contains a label(int) and an utterance(a list of dict)
 '''
 class speaker:
-    def __init__(self, label, utterance):
+    def __init__(self, label=None, utterance=None):
         self.label = label
         self.utterance = utterance
+    
+    def __str__(self):
+        return f'This is speaker {self.label} saying {self.utterance}'
 
 
 speakers = defaultdict(speaker)
+intervals = []
 
 # "results" is a list, and it's elements are intervals(dict)!
-print(data['results'][0]['alternatives'][0]['timestamps'])
+# the list of all intervals. We can calculate the time from it
+for interval in data['results']:
+    intervals.append(interval)
 
-# corresponds the text with speaker labels
+# for every timestamp in every interval, create a speaker object and use the start time as key
+for interval in data['results']:
+    for timestamp in interval['alternatives'][0]['timestamps']:
+        utterance = {'text': timestamp[0], 'from': timestamp[1], 'to': timestamp[2]}
+        speakers[timestamp[1]] = speaker(None, utterance)
 
+# corresponds the text with speaker labels, which is a list
+for label in data['speaker_labels']:
+    speakers[label['from']].label = label['speaker']
 
+for k, v in speakers.items():
+    print(f'{v}')
