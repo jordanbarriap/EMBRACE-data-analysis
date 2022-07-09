f27db6e0b2e0fc6d706699db8fc6a5fd33a4a325
Kangying <conniezi1008@gmail.com>
2022-07-08 21:03:54 -0400

    specify background_audio_suppression to adjust noise, and it gives a better result

--- ibm_test.py
+++ ibm_test.py
@@ -1,7 +1,9 @@
 # depentencies import
 from ibm_watson import SpeechToTextV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import speech_recognition as sr
 import json
+import os
 
 api_key = 'r1xA04K1GHDn8_sATEA5qcF8u1mSSOvm3IuyEJMXpR9W'
 url = 'https://api.au-syd.speech-to-text.watson.cloud.ibm.com/instances/bacb3fff-7963-483c-9197-e7feb05ad805'
@@ -10,10 +12,21 @@ authenticator = IAMAuthenticator(api_key)
 stt = SpeechToTextV1(authenticator=authenticator)
 stt.set_service_url(url)
 
-file_name = "/home/kangying/Desktop/Code/AudioProcessing/audios/record-672279722.51811.wav"
+file_name = 'audios/record-672279722.51811.flac'
+
+# recognizer = sr.Recognizer()
+# recognizer.energy_threshold = 4000
+
+
+# test_audio_file = sr.AudioFile(file_name)
+
+# # adjust for ambient noise pass in the file
+# with test_audio_file as source:
+#     recognizer.adjust_for_ambient_noise(source, duration = 0.5)
+#     audio_data = recognizer.record(source)
 
 with open(file_name, 'rb') as f:
-    res = stt.recognize(audio=f, content_type='audio/wav', speaker_labels=True, model='es-MX_NarrowbandModel').get_result()
+    res = stt.recognize(audio=f, content_type='audio/flac', speaker_labels=True, model='es-MX_NarrowbandModel', background_audio_suppression = 0.5).get_result()
 
-with open('ibm_transcript.json', 'w', encoding='utf-8') as f:
+with open('ibm_transcript_noise_suppression.json', 'w', encoding='utf-8') as f:
     json.dump(res, f, ensure_ascii=False, indent=4)
