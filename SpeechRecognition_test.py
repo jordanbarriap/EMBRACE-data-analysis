719d9382d91645d8412539503f29e6dc1c1d7820
Kangying <conniezi1008@gmail.com>
2022-07-19 16:43:18 -0400

    Speech recognition did the English part, but performed badly

--- SpeechRecognition_test.py
+++ SpeechRecognition_test.py
@@ -2,11 +2,11 @@ import speech_recognition as sr
 
 recognizer = sr.Recognizer()
 
-test_audio_file = sr.AudioFile('audios/audio_files_harvard.wav')
+test_audio_file = sr.AudioFile('audios/record-672279722.51811.wav')
 
 # adjust for ambient noise pass in the file
 with test_audio_file as source:
-    recognizer.adjust_for_ambient_noise(source, duration = 0.5)
+    # recognizer.adjust_for_ambient_noise(source)
     audio_data = recognizer.record(source)
-    result = recognizer.recognize_google(audio_data = audio_data)
+    result = recognizer.recognize_google(audio_data = audio_data, language='en-US')
     print(result)
