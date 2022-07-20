67d55f4d9c404ce03cfe8edd7d2a4f8cf5dda2c0
Kangying <conniezi1008@gmail.com>
2022-07-19 23:08:42 -0400

    Use Google API only for English

--- google_speaker_diarization.py
+++ google_speaker_diarization.py
@@ -1,10 +1,15 @@
 from google.cloud import speech_v1p1beta1 as speech
 import os
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/Users/zikangying/Desktop/Code/NLP/KEY.json"
+import sys
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "KEY.json"
 
 client = speech.SpeechClient()
 
-speech_file = "/Users/zikangying/Desktop/Code/NLP/audios/output.wav"
+speech_file = "audios/record-672279722.51811.flac"
+
+# first_lang = "en-US"
+# second_lang = "es"
 
 with open(speech_file, "rb") as audio_file:
     content = audio_file.read()
@@ -14,17 +19,19 @@ audio = speech.types.RecognitionAudio(content=content)
 diarization_config = speech.types.SpeakerDiarizationConfig(
   enable_speaker_diarization=True,
   min_speaker_count=3,
-  max_speaker_count=3,
+#   max_speaker_count=3,
 )
 
 
 config = speech.types.RecognitionConfig(
-    encoding=speech.types.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=48000,
-    language_code="en-US",
-    # diarization_config=diarization_config,
-    audio_channel_count=3,
-    enable_separate_recognition_per_channel=True,
+    encoding=speech.types.RecognitionConfig.AudioEncoding.FLAC,
+    sample_rate_hertz=44100,
+    language_code='en-US',
+    # alternative_language_codes=[second_lang],
+    diarization_config=diarization_config,
+    audio_channel_count=2,
+    # enable_separate_recognition_per_channel=True,
+    enable_word_confidence=True,
 )
 
 print("Waiting for operation to complete...")
@@ -38,6 +45,7 @@ result = response.results[-1]
 
 words_info = result.alternatives[0].words
 
+sys.stdout = open('google_En_output.txt', 'w')
 # Printing out the output:
 for word_info in words_info:
     print(
@@ -49,4 +57,11 @@ for i, result in enumerate(response.results):
     print("-" * 20)
     print("First alternative of result {}".format(i))
     print(u"Transcript: {}".format(alternative.transcript))
+    print(
+        u"First Word and Confidence: ({}, {})".format(
+            alternative.words[0].word, alternative.words[0].confidence
+        )
+    )
     print(u"Channel Tag: {}".format(result.channel_tag))
+
+sys.stdout.close()
