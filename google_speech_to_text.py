3e9f51259740b471d2eada77086f8750cf52f294
Kangying <conniezi1008@gmail.com>
2022-07-19 23:09:08 -0400

    update

--- google_speech_to_text.py
+++ google_speech_to_text.py
@@ -2,41 +2,19 @@
 from asyncio import subprocess
 from google.cloud import speech
 
-# Instantiates a client
-client = speech.SpeechClient.from_service_account_file('/Users/zikangying/Desktop/Code/NLP/KEY.json')
-
-file_name = 'NLP/audios/3_speakers.wav'
-
-with open(file_name, 'rb') as f:
-    audio_data = f.read()
-
-audio_file = speech.RecognitionAudio(content = audio_data)
-
-config = speech.RecognitionConfig(
-    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    #sample_rate_hertz=16000,
-    language_code="en-US",
-    enable_automatic_punctuation = True,
-)
-
-subprocess.call(['avconv', '-i', file_name, '-y', '-ar', '48000', '-ac', '1', 'last.flac'])
-
-# Detects speech in the audio file
-response = client.recognize(config=config, audio=audio_file)
-
-for result in response.results:
-    print("Transcript: {}".format(result.alternatives[0].transcript))
 
 """Transcribe the given audio file."""
 import os
 from google.cloud import speech
 import io
+import sys
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/zikangying/Desktop/Code/NLP/KEY.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "KEY.json"
 
+# Instantiates a client
 client = speech.SpeechClient()
 
-with io.open("/Users/zikangying/Desktop/Code/NLP/audios/3_speakers.wav", "rb") as audio_file:
+with io.open("audios/record-672279722.51811.flac", "rb") as audio_file:
     content = audio_file.read()
 
 audio = speech.RecognitionAudio(content=content)
@@ -48,16 +26,22 @@ diarization_config = speech.SpeakerDiarizationConfig(
 )
 
 config = speech.RecognitionConfig(
-    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=16000,
+    encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+    sample_rate_hertz=44100,
     language_code="en-US",
     diarization_config=diarization_config,
+    audio_channel_count=2,
+    enable_automatic_punctuation = True,
 )
 
+print("Waiting for operation to complete...")
 response = client.recognize(config=config, audio=audio)
 
 # Each result is for a consecutive portion of the audio. Iterate through
 # them to get the transcripts for the entire audio file.
+sys.stdout = open('google_En_output.txt', 'w')
 for result in response.results:
     # The first alternative is the most likely one for this portion.
     print(u"Transcript: {}".format(result.alternatives[0].transcript))
+
+sys.stdout.close()
