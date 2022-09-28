e6acfae2cefeff4756aa968e51e1550433531390
ConnieZi <conniezi1008@gmail.com>
2022-09-28 00:30:50 -0400

    remove ipad's voice in the second (dad and son) audio. Blue: dad, red: son. The beginning doesn't perform very well.

--- Diarization/slice_ipad.py
+++ Diarization/slice_ipad.py
@@ -9,12 +9,14 @@ import matplotlib.pyplot as plt
 from pyannote_test import diarization
 
 # Load the audio.
-song = AudioSegment.from_wav('audios/record-672279722.51811.wav')
+dir = os.path.dirname(os.getcwd())
+WAV_FILE= os.path.join(dir, 'audios/record-667269360.9571331.wav')
+song = AudioSegment.from_wav(WAV_FILE)
 
 # pydub does things in milliseconds
 # a list containing different lists of human voice chunks (removing the iPad chunks)
 # TODO: unreproducible hard-coded values
-human_seconds = [song[21 * 1000:51* 1000]]
+human_seconds = [song[35 * 1000:61* 1000], song[76 * 1000: ]]
 
 human_song = human_seconds[0]
 # iterate over the chunk, concatenate them one by one to a new audio
@@ -24,6 +26,6 @@ if len(human_seconds) > 1:
 
 # TODO: incorrect export path!!
 # export the new audio
-human_song.export("1_noipd.wav", format="wav")
+human_song.export("2_noipd.wav", format="wav")
 # diarize the audio without iPad voice
-diarization('1_noipd.wav', 'Figure_1_noipd.png')
+diarization('2_noipd.wav', 'Figure_2_noipd.png')
