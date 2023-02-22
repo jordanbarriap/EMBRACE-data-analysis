8d92b92e7f119cd73fdf20d5f137a66e74714d1b
ConnieZi <conniezi1008@gmail.com>
2023-02-22 15:59:57 -0500

    Get the CAR summary for each book based on the Tap/Decrement actions

--- ASU_Data/functions/XML_parser.py
+++ ASU_Data/functions/XML_parser.py
@@ -1,25 +1,63 @@
 '''
-    There are three types of questions
-    There are two types of actions: Tap (increment) and decrement
+    Calculate the CAR summary within a single file (might contain more than one book)
+    It's based on the Tap/Decrement actions
 '''
 from lxml import etree
 
-class question:
-    def __init__(self, name, count):
-        self.name = name
-        self.count = count
+class counter:
+    '''
+        A counter for each book that counts CAR questions
+    '''
+    def __init__(self, book_name):
 
-def count_question(q_type):
+        self.book_name = book_name
+
+        # There are three types of questions
+        self.concrete = 0
+        self.abstract = 0
+        self.relational = 0
+
+def get_books(root):
     '''
-        parameter(s):  a quesetion object 'q_type'
-        Modifying the count attribute directly
+        get the book(s) appeared in a single file file
     '''
-    name = q_type.name
-    for i in root.findall('.//Action'):
-        if i.text == f'Tap {name} ({name[0]}) Button':
-            q_type.count += 1
-        elif i.text == f'Decrement {name} ({name[0]}) Count':
-            q_type.count -= 1
+    book_list = []
+
+
+    for i in root.findall('.//Book_Title'):
+        if i.text is not None and i.text not in book_list:
+            book_list.append(i.text)
+    return book_list
+
+def confirm_context(element):
+    ancestor = element.getparent().getparent().getparent()
+    
+    if ancestor.tag == 'User_Action':
+        selection_child =  ancestor.find('Selection')
+        # locate this User_Action element
+        if selection_child is not None and selection_child.text == 'TapParentQuestionTypeEvent':
+            return True
+
+def count_q(counter, ancestor):
+    '''
+        In the context containing 'TapParentQuestionTypeEvent' action, we toggle the counter based on Tap/Decrement
+    '''
+    action_subelement = ancestor.find('Action')
+    for q_type in ['Concrete', 'Abstract', 'Relational']:
+        if action_subelement.text == f'Tap {q_type} ({q_type[0]}) Button':
+            if q_type == 'Concrete':
+                counter.concrete += 1
+            elif q_type == 'Abstract':
+                counter.abstract += 1
+            else:
+                counter.relational += 1
+        elif action_subelement.text == f'Decrement {q_type} ({q_type[0]}) Count':
+            if q_type == 'Concrete':
+                counter.concrete -= 1
+            elif q_type == 'Abstract':
+                counter.abstract -= 1
+            else:
+                counter.relational -= 1
 
 
 if __name__=='__main__': 
@@ -28,14 +66,22 @@ if __name__=='__main__':
     tree = etree.parse(xml_path)
     root = tree.getroot()
 
-    # create three question objects
-    concrete = question('Concrete', 0)
-    abstract = question('Abstract', 0)
-    relational = question('Relational', 0)
-    q_types = [concrete, abstract, relational]
+    book_list = get_books(root)
 
-    aggregate_count = []
-    for q_type in q_types:
-        count_question(q_type)
-        aggregate_count.append(q_type.count)
-    print(aggregate_count)
+    counter_list = []
+    for book in book_list:
+        # create a counter for each book in the book_list
+        counter_list.append(counter(book))
+    
+    for counter in counter_list:
+        for i in root.findall('.//Book_Title'):
+                # once find an element about this book which is also within the context containg 'TapParentQuestionTypeEvent'
+                if i.text == counter.book_name and confirm_context(i):
+                        # we count this snippit
+                        count_q(counter, i.getparent().getparent().getparent())
+    
+    for counter in counter_list:
+        print(f'{counter.book_name}: {counter.concrete}, {counter.abstract}, {counter.relational}')
+
+
+    
--- ASU_Data/functions/XML_parser_folder.py
+++ ASU_Data/functions/XML_parser_folder.py
@@ -1,42 +1,102 @@
+'''
+    There are two types of actions: Tap (increment) and decrement
+    Different books have different counters!
+'''
+
 from lxml import etree
 import os
 
-class question:
-    def __init__(self, name, count):
-        self.name = name
-        self.count = count
+class counter:
+    '''
+        A counter for each book that has three question objects for CAR questions
+    '''
+    def __init__(self, book_name):
 
-def count_question(root, q_type):
-    '''
-        parameter(s):  a quesetion object 'q_type'
-        Modifying the count attribute directly
-    '''
-    name = q_type.name
-    for i in root.findall('.//Action'):
-        if i.text == f'Tap {name} ({name[0]}) Button':
-            q_type.count += 1
-        elif i.text == f'Decrement {name} ({name[0]}) Count':
-            q_type.count -= 1
+        self.book_name = book_name
 
-def process_xml_file(q_types):
+        #  There are three types of questions
+        self.concrete = 0
+        self.abstract = 0
+        self.relational = 0
+
+def get_books():
     '''
-        Dealing with each xml file under this folder
+        Iterate thru all the files to get a list of all the books this user reads
+
+        return: 
+            a list of all the book this user reads
     '''
-    # A list containing 3 counters for CAR questions
-    aggregate_count = []
+    book_list = []
 
     for xml_file in dir_list:
         # parse each xml file
         tree = etree.parse(dir_path + '/' + xml_file)
         root = tree.getroot()
+
+        for i in root.findall('.//Book_Title'):
+            if i.text is not None and i.text not in book_list:
+                book_list.append(i.text)
+    return book_list
+
+
+def confirm_context(element):
+    ancestor = element.getparent().getparent().getparent()
+    
+    if ancestor.tag == 'User_Action':
+        selection_child =  ancestor.find('Selection')
+        # locate this User_Action element
+        if selection_child is not None and selection_child.text == 'TapParentQuestionTypeEvent':
+            return True
+
+def count_q(counter, ancestor):
+    '''
+        In the context containing 'TapParentQuestionTypeEvent' action, we toggle the counter based on Tap/Decrement
+
+        parameters:
+            counter: a counter for a specific book
+            ancestor: the 'User_Action' element that contains the 'TapParentQuestionTypeEvent' subelement
+    '''
+    action_subelement = ancestor.find('Action')
+    for q_type in ['Concrete', 'Abstract', 'Relational']:
+        if action_subelement.text == f'Tap {q_type} ({q_type[0]}) Button':
+            if q_type == 'Concrete':
+                counter.concrete += 1
+            elif q_type == 'Abstract':
+                counter.abstract += 1
+            else:
+                counter.relational += 1
+        elif action_subelement.text == f'Decrement {q_type} ({q_type[0]}) Count':
+            if q_type == 'Concrete':
+                counter.concrete -= 1
+            elif q_type == 'Abstract':
+                counter.abstract -= 1
+            else:
+                counter.relational -= 1
+
+
+def process_xml_file(counter_list):
+    '''
+        Dealing with each xml file under this folder and modify counter_list in-place
+
+        parameter: 
+            counter_list: a list containing all the counters for all the books
+    '''
+    # A list containing 3 counters for CAR questions
+    #aggregate_count = []
+
+    for xml_file in dir_list:
+        # parse each xml file
+        tree = etree.parse(dir_path + '/' + xml_file)
+        root = tree.getroot()
+
+        # send books(counters) one by one
+        for counter in counter_list:
+            for i in root.findall('.//Book_Title'):
+                # for an element about this book which is also within the context we want to find
+                if i.text == counter.book_name and confirm_context(i):
+                        # we count it
+                        count_q(counter, i.getparent().getparent().getparent())
         
-        # Modify the counter of each type of question
-        for q_type in q_types:
-            count_question(root, q_type)
-        
-    for q_type in q_types:
-        aggregate_count.append(q_type.count)
-    return aggregate_count
 
 
 if __name__=='__main__': 
@@ -44,12 +104,14 @@ if __name__=='__main__':
     dir_path = 'ASU_Data/par 007'
     dir_list = [file for file in os.listdir(dir_path) if os.path.splitext(file)[1] == '.xml']
 
-    # create three question objects for this user (count all the files)
-    concrete = question('Concrete', 0)
-    abstract = question('Abstract', 0)
-    relational = question('Relational', 0)
-    q_types = [concrete, abstract, relational]
+    # get a set of books
+    book_list = get_books()
 
-    # each file will modify the global counter
-    aggregate_count = process_xml_file(q_types)
-    print(aggregate_count)
+    counter_list = []
+    for book in book_list:
+        # create a counter for each book in the book_list
+        counter_list.append(counter(book))
+
+    process_xml_file(counter_list)
+    for counter in counter_list:
+        print(f'{counter.book_name}: {counter.concrete}, {counter.abstract}, {counter.relational}')
