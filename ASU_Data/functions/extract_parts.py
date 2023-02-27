4f9127b07aaa6e3171a7f64df411d01df4714fb1
ConnieZi <conniezi1008@gmail.com>
2023-02-27 12:01:54 -0500

    Extract label parts and tap/decrement parts to one xml file

---
+++ ASU_Data/functions/extract_parts.py
@@ -0,0 +1,49 @@
+from lxml import etree
+
+def extract_file(root, f):
+    # iterate thru all the elements
+    for element in root.iter():
+        # CAR labels
+        if element.tag == 'Input' and len(element) > 0 and  element[0].tag == 'Concrete':
+            f.write("\n<Input>\n")
+            print("<Input>")
+            for i in element:
+                f.write(f'  <{i.tag}> {i.text}  </{i.tag}>\n')
+                print(f'    <{i.tag}> {i.text}  </{i.tag}>')
+            
+            f.write("</Input>\n\n")
+            print("</Input>\n")
+        
+        # tap/decrement events
+        if element.text == 'TapParentQuestionTypeEvent':
+            action_element = element.getparent().find('Action')
+            f.write(f'  <{action_element.tag}> {action_element.text}  </{action_element.tag}>\n')
+            print(f'    <{action_element.tag}> {action_element.text}  </{action_element.tag}>\n')
+        
+        # CAR labels with language separation
+        if element.tag == 'QuestionTypeCounts':
+            f.write("\n<QuestionTypeCounts>\n")
+            for i in element:
+                f.write(f'   <{i.tag}>\n')
+                for sub_i in i:
+                    f.write(f"   <{sub_i.tag}> {sub_i.text} </{sub_i.tag}>\n")
+                f.write(f'</{i.tag}>\n')
+            f.write("</QuestionTypeCounts>\n\n")
+
+
+def extract_folder():
+    pass
+
+
+if __name__=='__main__': 
+    xml_path = 'ASU_Data/par 007/5 par007 e 10-19-2022T07_13.13.606.xml'
+    folder_path = 'ASU_Data/par 007'
+
+    tree = etree.parse(xml_path)
+    # root for one single xml file
+    root = tree.getroot()
+
+
+    f = open('ASU_Data/functions/test.xml', "a")
+    extract_file(root, f)
+    f.close()
